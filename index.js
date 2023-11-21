d7bb0d44f91dae43b7aaa1568651a9074e90ec4d
Angelique Abacajan <ma_utsey@yahoo.com>
2023-11-21 08:14:38 +0800

    Implemented a callback function for the DNS and utilizing async/await syntax to handle async database queries and DNS

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const { MongoClient } = require("mongodb");
 const dns = require("dns")
@@ -24,8 +24,26 @@ app.get("/", function(req, res) {
 });
 
 // Your first API endpoint
-app.get("/api/hello", function(req, res) {
-  res.json({ greeting: "hello API" });
+app.post("/api/shorturl", function(req, res) {
+  
+    console.log(req.body)
+    const url = req.body.url
+    const dnslookup = dns.lookup(urlparser.parse(url).hostname, async (err, address) => {
+        if (!address){
+            res.json({error: "Invalid URL"})
+        } else {
+            const urlCount = await urls.countDocuments({})
+            const urlDoc = {
+                url,
+                short_url: urlCount
+            }
+
+          const result = await urls.insertOne(urlDoc)
+          console.log(result);
+          res.json({ original_url: url, short_url: urlCount })
+          
+        }
+    })
 });
 
 app.listen(port, function() {
