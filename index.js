2badb275ffd1034f301804d49f99cd905cf933e5
Angelique Abacajan <ma_utsey@yahoo.com>
2023-11-21 08:05:24 +0800

    Implemented variables urls, db, and client

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient } = require('mongodb');
-const dns = require('dns')
-const urlparser = require('url')
+const { MongoClient } = require("mongodb");
+const dns = require("dns")
+const urlparser = require("url") 
+
+const client = new MongoClient(process.env.DB_URL)
+const db = client.db("urlshortner")
+const urls = db.collection("urls")
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: "hello API" });
 });
 
 app.listen(port, function() {
